37ca35c0a2fa3b9e7dc5357b1e15705803a2c041
Feliganso <83733448+Feliganso@users.noreply.github.com>
2021-05-28 06:55:51 -0500

    Create Anuncio.js

---
+++ js/Anuncio.js
@@ -0,0 +1,12 @@
+
+        function getValuser()
+        {
+            var txt = document.getElementById('user').value;
+            alert("Bienvenido " +txt);
+        }
+        
+                function getValadmin()
+        {
+            var txt = document.getElementById('emp').value;
+            alert("Bienvenido " +txt);
+        }
