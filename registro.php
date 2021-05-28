c24bf219a2c0ddc2daa07fe4178aa1b1d935e236
Feliganso <83733448+Feliganso@users.noreply.github.com>
2021-05-28 14:22:28 -0500

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,34 @@
+<?php
+	
+	$conexion=mysql_connect("localhost","root","","usuarios nuevos") or die("<h2>No se encuentra el servidor</h2>");
+
+	
+	$nombres = $_POST['nom'];
+	$apellidos = $_POST['ap'];
+    $usuario = $_POST['us'];
+	$email = $_POST['correo'];
+	$pass = $_POST['contra'];
+	$rpass = $_POST['contrar'];
+
+	
+	$req = (strlen($nombres)*strlen($apellidos)*strlen($email)*strlen($pass)*strlen($rpass)) or die("No se han llenado todos los campos");
+
+
+	if ($pass != $rpass) {
+		die('Las contraseñas no coinciden, Verifique <br /> <a href="Registro.html">Volver</a>');
+	}
+
+	
+	$contraseñaUser = md5($pass);
+
+	
+	mysql_query("INSERT INTO usuarios nuevos VALUES('','$nombres','$apellidos','$usuario''$email','$pass')",$link) or die("<h2>Error Guardando los datos</h2>");
+	echo'
+		<script>
+			alert("Registro Exitoso");
+			location.href="index.html";
+		</script>
+	'
+
+
+?>
